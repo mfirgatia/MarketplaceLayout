acd5d21beeba322565b840a8938bbc4b428653e5
mfirgatia <muktifirgatia@gmail.com>
2020-02-11 22:00:41 +0700

    Merubah tata letak next pada general info

--- app/src/main/java/id/putraprima/marketplacelayout/Main4Activity.java
+++ app/src/main/java/id/putraprima/marketplacelayout/Main4Activity.java
@@ -2,7 +2,9 @@ package id.putraprima.marketplacelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main4Activity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Main4Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+    }
+    public void Next(View view) {
+        Intent iten = new Intent(Main4Activity.this, Main2Activity.class);
+        startActivity(iten);
     }
 }
