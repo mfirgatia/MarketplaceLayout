24cb689423d5b3551274c8bc80bddd843e07b454
mfirgatia <muktifirgatia@gmail.com>
2020-02-11 21:48:53 +0700

    Menambahkan Fungsi onclick

--- app/src/main/java/id/putraprima/marketplacelayout/Main2Activity.java
+++ app/src/main/java/id/putraprima/marketplacelayout/Main2Activity.java
@@ -2,7 +2,9 @@ package id.putraprima.marketplacelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+    public void Next(View view) {
+        Intent iten = new Intent(Main2Activity.this, Main3Activity.class);
+        startActivity(iten);
+    }
+    public void Next1(View view) {
+        Intent iten = new Intent(Main2Activity.this, Main4Activity.class);
+        startActivity(iten);
     }
 }
--- app/src/main/java/id/putraprima/marketplacelayout/Main3Activity.java
+++ app/src/main/java/id/putraprima/marketplacelayout/Main3Activity.java
@@ -2,7 +2,9 @@ package id.putraprima.marketplacelayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -11,4 +13,9 @@ public class Main3Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
     }
+    public void Next(View view) {
+        Intent iten = new Intent(Main3Activity.this, Main2Activity.class);
+        startActivity(iten);
+    }
+}
 }
